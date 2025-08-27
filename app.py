1b108ce9d368ca727c50f4a86f8c906035c9cdd7
Ashutosh Patidar <160703433+Ashutosh-patidar47@users.noreply.github.com>
2025-08-27 11:24:21 +0530

    app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ from email.mime.multipart import MIMEMultipart
 # -----------------------------------
 # 🔑 Load secrets from Streamlit Cloud
 # -----------------------------------
-WEATHER_API_KEY=60d975cfef77aedca8bab27b843bafb6
-EMAIL_ADDRESS=weatherforecast386@gmail.com
-EMAIL_PASSWORD=wemgsufmpvoucjji
+WEATHER_API_KEY="60d975cfef77aedca8bab27b843bafb6"
+EMAIL_ADDRESS="weatherforecast386@gmail.com"
+EMAIL_PASSWORD="wemgsufmpvoucjji"
 SMTP = "smtp.gmail.com"
 PORT = 587
 
@@ -111,6 +111,7 @@ if st.button("Get Forecast"):
                     st.error(f"❌ Failed to send email: {status}")
         else:
             st.error("City not found. Please try again.")
+
 
 
 
